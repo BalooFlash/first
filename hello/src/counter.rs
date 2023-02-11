2cfffc58ac83fc58f9fea6219954ddba2966b77e
DripMicro <whappydrip@gmail.com>
2023-02-11 11:33:05 +0300

    Login our sever traffic

--- hello/src/counter.rs
+++ hello/src/counter.rs
@@ -14,11 +14,43 @@ impl Counter {
             .unwrap().to_owned()
     }
 
-    pub fn save(self) {
+    pub fn save(self) -> Result<(), redis::RedisError> {
+        let serialized = serde_yaml::to_vec(&self).unwrap();
 
+        let client = match redis::Client::open(
+            Counter::get_redis_url()
+        )  {
+            Ok(client) => client,
+            Err(error) => return Err(error)
+        };
+        let mut con = match client.get_connection() {
+            Ok(con) => con,
+            Err(error) => return Err(error)
+        };
+        match redis::cmd("SET").arg("COUNTER")
+                                .arg(serialized)
+                                .query::<Vec<u8>>(&mut con) {
+                                    Ok(_) => Ok(()),
+                                    Err(error) => Err(error)
+                                }
     }
 
-    pub fn load() -> Counter {
-
-    }
+    pub fn load() -> Result<Counter, redis::RedisError> {
+        let client = match redis::Client::open(
+            Counter::get_redis_url()){
+            Ok(client) => client,
+            Err(error) => return Err(error)
+        };
+        let mut con = match client.get_connection() {
+            Ok(con) => con,
+            Err(error) => return Err(error)
+        };
+        let byte_data: Vec<u8> = match redis::cmd("GET")
+                                            .arg("COUNTER")
+                                            .query(&mut con) {
+                                                Ok(data) => data,
+                                                Err(error) => return Err(error)
+                                            };
+        Ok(serde_yaml::from_slice(&byte_data).unwrap())
+    } 
 } 
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -16,41 +16,103 @@ mod json_serialization;
 mod jwt;
 mod models;
 mod config;
+mod counter;
 
+
+// #[actix_web::main]
+// async fn main() -> std::io::Result <()> {
+//     const ALLOWED_VERSION: &'static str = include_str!("./output_data.txt");
+    
+//     let mut site_counter = counter::Counter{count: 0};
+//     site_counter.save();
+//     println!("first");
+//     HttpServer::new( || {
+//         let cors = Cors::default().allow_any_origin()
+//                                 .allow_any_method()
+//                                 .allow_any_header();
+//     println!("second");
+
+//         let app = App::new()
+//             .wrap_fn(|req, srv| {
+//                 let passed: bool;
+//     println!("third");
+                
+//                 let mut site_counter = counter::Counter::load().unwrap();
+//                 // site_counter += 1;
+//                 println!("site_counter: {:?}", &site_counter);
+//                 site_counter.save();
+
+//                 if *&req.path().contains(&format!("/{}/", ALLOWED_VERSION)) {
+//                     passed = true;
+//                 } else {
+//                     passed = false;
+//                 }
+//                 println!("{}", passed);
+//                 let end_result;
+//                 if passed == true {
+//                     end_result = Either::Left(srv.call(req));
+//                 }else {
+//                     let resp = HttpResponse::NotImplemented().body(format!("only {} API is no longer supported", ALLOWED_VERSION));
+//                     end_result = Either::Right(ok(req.into_response(resp).map_into_boxed_body()))
+//                 }
+//                 async move {
+//                     let result = end_result.await?;
+//                     Ok(result)
+//                 }
+//             }).configure(views::views_factory).wrap(cors);
+//             return app
+//     })
+//     .bind("127.0.0.1:8000")?
+//     .run()
+//     .await
+// }
 
 #[actix_web::main]
-async fn main() -> std::io::Result <()> {
+async fn main() -> std::io::Result<()> {
+
     const ALLOWED_VERSION: &'static str = include_str!("./output_data.txt");
-    println!("{:?}", ALLOWED_VERSION);
-    HttpServer::new( || {
-        let cors = Cors::default().allow_any_origin()
-                                .allow_any_method()
-                                .allow_any_header();
+    let site_counter = counter::Counter{count: 0};
+    let _ = site_counter.save();
+
+    HttpServer::new(|| {
+        let cors = Cors::default().allow_any_origin().allow_any_method().allow_any_header();
         let app = App::new()
-            .wrap_fn(|req, srv| {
+            .wrap_fn(|req, srv|{
+
                 let passed: bool;
+                let mut site_counter = counter::Counter::load().unwrap();
+                site_counter.count += 1;
+                println!("{:?}", &site_counter);
+                let _ = site_counter.save();
 
                 if *&req.path().contains(&format!("/{}/", ALLOWED_VERSION)) {
                     passed = true;
                 } else {
                     passed = false;
                 }
-                println!("{}", passed);
-                let end_result;
-                if passed == true {
-                    end_result = Either::Left(srv.call(req));
-                }else {
-                    let resp = HttpResponse::NotImplemented().body(format!("only {} API is no longer supported", ALLOWED_VERSION));
-                    end_result = Either::Right(ok(req.into_response(resp).map_into_boxed_body()))
-                }
+
+                println!("{:?}", req);
+                let end_result = match passed {
+                    true => {
+                        Either::Left(srv.call(req))
+                    },
+                    false => {
+                        let resp = HttpResponse::NotImplemented().body(format!(
+                            "only {} API is supported", ALLOWED_VERSION
+                        ));
+                        Either::Right(
+                            ok(req.into_response(resp).map_into_boxed_body())
+                        )
+                    }
+                };
                 async move {
                     let result = end_result.await?;
                     Ok(result)
                 }
             }).configure(views::views_factory).wrap(cors);
-            return app
+        return app
     })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
