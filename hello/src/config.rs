a6ee48597baeb7522c87c20b623388885851526e
DripMicro <whappydrip@gmail.com>
2023-02-12 03:36:45 +0300

    Success actix_web and create rocket_app

--- hello/src/config.rs
+++ hello/src/config.rs
@@ -9,6 +9,8 @@ pub struct Config {
 }
 
 impl Config {
+    // EXISTING FUNCTION READING FROM FILE
+    #[cfg(not(test))]
     pub fn new() -> Config {
         let args: Vec<String> = env::args().collect();
         let file_path = &args[args.len() - 1];
@@ -17,5 +19,24 @@ impl Config {
         let map: HashMap<String, serde_yaml::Value> = serde_yaml::
         from_reader(file).unwrap();
         return Config{map}
+    }
+
+    // NEW FUNCTION FOR TESTING
+    #[cfg(test)]
+
+    pub fn new() -> Config {
+        let mut map = HashMap::new();
+        map.insert(String::from("DB_URL"),
+                   serde_yaml::from_str(
+                       "postgres://flash:flash@localhost:5432/to_do")
+                       .unwrap());
+        map.insert(String::from("SECRET_KEY"),
+                   serde_yaml::from_str("secret").unwrap());
+        map.insert(String::from("EXPIRE_MINUTES"),
+                   serde_yaml::from_str("120").unwrap());
+        map.insert(String::from("REDIS_URL"),
+                   serde_yaml::from_str("redis://127.0.0.1/").unwrap());
+        return Config {map}
+
     }
 }
